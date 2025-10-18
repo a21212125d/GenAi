6189d6e7bcf2af90d98cc2721707365d93ef16db
d <odishapublic80@gmail.com>
2025-10-18 19:54:53 +0530

    refactor: Update app title and README
    Update the application title in index.html to "AI Creative Suite" and refresh the README.md file with new branding and updated instructions.

--- components/icons.jsx
+++ components/icons.jsx
@@ -12,21 +12,3 @@ export const SparklesIcon = ({ className = 'w-5 h-5' }) => (
         <path fillRule="evenodd" d="M9 4.5a.75.75 0 01.721.544l.813 2.846a3.75 3.75 0 002.846.813l2.846-.813a.75.75 0 01.544.721v2.846l-.813 2.846a3.75 3.75 0 00.813 2.846l2.846.813a.75.75 0 01.721.544v.813l-2.846.813a3.75 3.75 0 00-2.846.813l-.813 2.846a.75.75 0 01-.544.721h-.813l-.813-2.846a3.75 3.75 0 00-2.846-.813l-2.846.813a.75.75 0 01-.721-.544v-.813l2.846-.813a3.75 3.75 0 00.813-2.846l.813-2.846V7.221a.75.75 0 01.721-.544zM4.125 9.375a.75.75 0 01.75 0l1.5 1.5a.75.75 0 010 1.06l-1.5 1.5a.75.75 0 01-1.06 0l-1.5-1.5a.75.75 0 010-1.06l1.5-1.5a.75.75 0 01.75 0zm14.625 1.875a.75.75 0 010 1.06l-1.5 1.5a.75.75 0 01-1.06 0l-1.5-1.5a.75.75 0 010-1.06l1.5-1.5a.75.75 0 011.06 0l1.5 1.5z" clipRule="evenodd" />
     </svg>
 );
-
-export const VideoIcon = ({ className = 'w-5 h-5' }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" className={className} viewBox="0 0 24 24" fill="currentColor">
-        <path fillRule="evenodd" d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.647c1.295.742 1.295 2.545 0 3.286L7.279 20.99c-1.25.717-2.779-.217-2.779-1.643V5.653z" clipRule="evenodd" />
-    </svg>
-);
-
-export const KeyIcon = ({ className = 'w-5 h-5' }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 5.25a3 3 0 013 3m3 0a6 6 0 01-7.029 5.912c-.563-.097-1.159.026-1.563.43L10.5 17.25H8.25v2.25H6v2.25H2.25v-2.818c0-.597.237-1.17.659-1.591l6.499-6.499c.404-.404.527-1 .43-1.563A6 6 0 1121.75 8.25z" />
-    </svg>
-);
-
-export const InfoIcon = ({ className = 'w-5 h-5' }) => (
-    <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
-    </svg>
-);
